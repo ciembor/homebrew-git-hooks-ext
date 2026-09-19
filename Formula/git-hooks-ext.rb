5fd59ad900e2f857017573f5b23474debf63d6fa
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2026-09-19 15:04:00 +0200

    Update git-hooks-ext to 0.2.0

--- Formula/git-hooks-ext.rb
+++ Formula/git-hooks-ext.rb
@@ -1,9 +1,9 @@
 class GitHooksExt < Formula
   desc "Semantic Git hooks for reference changes"
   homepage "https://github.com/ciembor/git-hooks-ext"
-  url "https://github.com/ciembor/git-hooks-ext/releases/download/v0.1.0/git-hooks-ext-0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "594c70215f9a660b5d9d0281930e1cf0e6fd93f62d91460a1213d59f11159a5d"
+  url "https://github.com/ciembor/git-hooks-ext/releases/download/v0.2.0/git-hooks-ext-0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "6004daf8653adfa87a4e298a9c713c79f8b30caedd67570023cd5eacbe0896cb"
   license "GPL-2.0-only"
 
   uses_from_macos "git"
